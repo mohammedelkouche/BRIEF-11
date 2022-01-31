65216f8eb50bd59673986118301ad5f200fdd6c0
mohammedelkouche <mohammed0elkouche@gmail.com>
2022-01-31 16:54:20 +0100

    ajouter les options de calibre

--- projet-11/interface.php
+++ projet-11/interface.php
@@ -8,16 +8,16 @@
 </head>
 <body>
     <form action="interface.php" method = "POST" >
-        <label  for="">l'ancien index</label> 
-            <input type="text" name ="ancien" placeholder= "l'ancien index">
-        <label  for="">le nouvel index</label> 
-            <input type="text" name = "nouvel" placeholder= "le nouvel index">
-        <label  for="">type du compteure</label> 
+        <label  for="">Ancien index</label> 
+            <input type="text" name ="Ancien" placeholder= "Ancien index">
+        <label  for="">Nouvel index</label> 
+            <input type="text" name = "Nouvel" placeholder= "Nouvel index">
+        <label  for="">Type du compteure</label> 
             <select name="select" id="">
-                <option disabled hidden selected value="">calibre</option>
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
+                <option disabled hidden selected value="">Calibre</option>
+                <option value="">5-10</option>
+                <option value="">15-20</option>
+                <option value="">>30</option>
             </select>
         <div>
             <input type="submit" name ="envoyer" value="calculer le totale de ma facture">
@@ -28,12 +28,47 @@
 
 
     <?php
-           if(isset($_POST['envoyer'])){
-            $Ancien_index = $_POST["ancien"] ;
-            $Nouvel_index = $_POST["nouvel"] ;
-            // $Consommation = $Nouvel_index - $Ancien_index  ;
+
+            $tranches = array() ;
+ 
+            // Associative Arrays 
+            $redevance = array(
+                "tarif_1" => 0.794 ;
+                "tarif_2" => 0.883 ;
+                "tarif_3" => 0.9451 ;
+                "tarif_4" => 1.04889 ;
+                "tarif_5" => 1.2915 ;
+                "tarif_6" => 1.4975 ;
+            ) 
+            
+            class Tache {
+
+                // constructeur
+                public function__construct($BornMin,$BornMax,$Tarif){
+                    $this -> 
+                }
             }
-    
+            
+            // tranche1 = new Tache (0,100,0.794);
+            // tranche2 = new Tache (101,150,0.794);
+            // tranche3 = new Tache (0,100,0.794);
+            // tranche4 = new Tache (0,100,0.794);
+            // tranche5 = new Tache (0,100,0.794);
+            // tranche6 = new Tache (0,100,0.794);
+
+            //  push dans le tableaux $tranches 
+
+
+           if(isset($_POST['envoyer'])){
+            $Ancien_index = $_POST["Ancien"] ;
+            $Nouvel_index = $_POST["Nouvel"] ;
+            $Consommation = $Nouvel_index - $Ancien_index  ;
+            // echo $Ancien_index ;   
+            // echo $Nouvel_index ;
+            // echo  $Consommation ;
+              
+
+            }
     ?>
 
 </body>
